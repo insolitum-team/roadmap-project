0382d9fd0482211515fe5edd4c2d066c74e3228f
ImmortalDante <maklakovfdr@gmail.com>
2023-08-29 16:39:52 +0400

    feature/tests-init

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 bash -c ./check-migrations.sh
-poetry run uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
+poetry run uvicorn api.main:app --reload --host 0.0.0.0 --port 8000
 exec "$@"
---
+++ tests/conftest.py
@@ -0,0 +1,47 @@
+import asyncio
+from typing import AsyncGenerator
+
+import pytest
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+
+from api.main import app
+from common.db.database import Base, get_async_session
+from common.settings.config import TEST_DATABASE_URL
+
+engine_test = create_async_engine(TEST_DATABASE_URL, poolclass=NullPool)
+async_session_maker = sessionmaker(
+    engine_test, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def override_get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+app.dependency_overrides[get_async_session] = override_get_async_session
+
+
+@pytest.fixture(autouse=True, scope="session")
+async def prepare_database():
+    async with engine_test.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    async with engine_test.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest.fixture(scope="session")
+def event_loop(request):
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="session")
+async def ac() -> AsyncGenerator[AsyncClient, None]:
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        yield ac
