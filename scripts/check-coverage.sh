ebc5c81a38395ed52e3a198550e7fefa399b922d
buhovets <x10c@duck.com>
2023-09-01 17:32:35 +0200

    coverage reduce

--- scripts/check-coverage.sh
+++ scripts/check-coverage.sh
@@ -7,8 +7,8 @@ pytest --cov > coverage_report.txt
 coverage=$(awk '/TOTAL/ {print $NF}' coverage_report.txt)
 
 # Check if coverage is below 90%
-if (( $(awk 'BEGIN {print ("'$coverage'" < 90)}') )); then
-  echo "Error: Test coverage is below 90% (${coverage})"
+if (( $(awk 'BEGIN {print ("'$coverage'" < 80)}') )); then
+  echo "Error: Test coverage is below 80% (${coverage})"
   exit 1
 else
   echo "Test coverage is acceptable (${coverage})"
