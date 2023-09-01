4c7d7939ab839b93cf8ff4d720c279891c5da005
ImmortalDante <maklakovfdr@gmail.com>
2023-09-01 17:16:55 +0400

    feature/tests-coverage

---
+++ scripts/check-coverage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run pytest with coverage and store the result in a temporary file
+pytest --cov > coverage_report.txt
+
+# Extract the coverage percentage from the report
+coverage=$(awk '/TOTAL/ {print $NF}' coverage_report.txt)
+
+# Check if coverage is below 90%
+if (( $(awk 'BEGIN {print ("'$coverage'" < 90)}') )); then
+  echo "Error: Test coverage is below 90% (${coverage})"
+  exit 1
+else
+  echo "Test coverage is acceptable (${coverage})"
+  exit 0
+fi
