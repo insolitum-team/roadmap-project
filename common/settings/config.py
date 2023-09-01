beca5968fd6c1beb9a57bce13748981bcfa8d681
buhovets <x10c@duck.com>
2023-09-01 21:12:40 +0200

    origins processing fix

--- common/settings/config.py
+++ common/settings/config.py
@@ -27,4 +27,4 @@ JWT_SECRET = os.getenv("JWT_SECRET")
 JWT_ACCESS_TTL = int(os.getenv("JWT_ACCESS_TTL"))
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
 
-CORS_ORIGINS = json.loads(os.getenv("CORS_ORIGINS", default="*"))
+CORS_ORIGINS = json.loads(os.getenv("CORS_ORIGINS"))
