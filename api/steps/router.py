a81420ffcab7e94894d7d2302c5e5b5243939282
Andrew Bukhovets <andrew.bukhovets@solveit.dev>
2023-11-10 09:33:20 +0100

    steps crud added

--- api/steps/router.py
+++ api/steps/router.py
@@ -1,3 +1,102 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from api.auth.service import get_current_user
+from api.steps.service import StepService
+from common.db.schemas.step import StepCreateModel, StepUpdateModel
 
-router = APIRouter(tags=["steps"])
+from common.db.schemas.user import UserModel
+
+router = APIRouter(prefix="/roadmaps", tags=["steps"])
+
+
+@router.post("")
+async def create_category(
+    data: StepCreateModel,
+    user: UserModel = Depends(get_current_user),
+    service: StepService = Depends(),
+):
+    try:
+        data = await service.create_step(data=data, user=user)
+        return {
+            "status": "success",
+            "data": data,
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e),
+        }
+
+
+@router.get("/step/{id}")
+async def get_step(
+    id: int,
+    user: UserModel = Depends(get_current_user),
+    service: StepService = Depends(),
+):
+    try:
+        data = await service.get_step(id=id, user=user)
+        return {
+            "status": "success",
+            "data": data,
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e),
+        }
+
+
+@router.get("")
+async def get_steps(
+    user: UserModel = Depends(get_current_user), service: StepService = Depends()
+):
+    try:
+        data = await service.get_steps(user=user)
+        return {
+            "status": "success",
+            "data": data,
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e),
+        }
+
+
+@router.put("/step/{id}")
+async def update_step(
+    id: int,
+    data: StepUpdateModel,
+    user: UserModel = Depends(get_current_user),
+    service: StepService = Depends(),
+):
+    try:
+        data = await service.update_step(id=id, data=data, user=user)
+        return {
+            "status": "success",
+            "data": data,
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e),
+        }
+
+
+@router.delete("/step/{id}")
+async def delete_step(
+    id: int,
+    user: UserModel = Depends(get_current_user),
+    service: StepService = Depends(),
+):
+    try:
+        data = await service.delete_step(id=id, user=user)
+        return {
+            "status": "success",
+            "data": data,
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e),
+        }
--- api/steps/service.py
+++ api/steps/service.py
@@ -1,11 +1,67 @@
 from fastapi import Depends
-from common.db.crud import StepsCRUD
+from sqlalchemy import select
+from api.steps.exceptions import StepNotFoundException
 
 from common.db.database import get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
+from common.db.models.step import Step
+from common.db.schemas.step import StepCreateModel, StepUpdateModel
+
+from common.db.schemas.user import UserModel
 
 
-class StepsService:
+class StepService:
     def __init__(self, session: AsyncSession = Depends(get_async_session)):
         self.session = session
-        self.crud = StepsCRUD(session)
+
+    async def create_step(self, data: StepCreateModel, user: UserModel) -> Step:
+        roadmap = Step(
+            title=data.title,
+            description=data.description,
+            roadmap_id=data.roadmap_id,
+        )
+        self.session.add(roadmap)
+        await self.session.commit()
+        return roadmap
+
+    async def get_step(self, id: int, user: UserModel) -> Step:
+        stmt = select(Step).filter(Step.id == id)
+        result = await self.session.execute(stmt)
+        return_step = result.scalars().first()
+        if not return_step:
+            StepNotFoundException()
+        return return_step
+
+    async def get_steps(self, user: UserModel) -> list[Step]:
+        stmt = select(Step)
+        result = await self.session.execute(stmt)
+        steps = result.scalars().all()
+        if not steps:
+            raise StepNotFoundException(
+                detail="No steps found",
+            )
+        return steps
+
+    async def update_step(
+        self, id: int, data: StepUpdateModel, user: UserModel
+    ) -> Step:
+        stmt = select(Step).filter(Step.id == id)
+        result = await self.session.execute(stmt)
+        return_step = result.scalars().first()
+        if not return_step:
+            raise StepNotFoundException()
+        return_step.title = data.title
+        return_step.description = data.description
+        return_step.roadmap_id = data.roadmap_id
+        await self.session.commit()
+        return return_step
+
+    async def delete_step(self, id: int, user: UserModel) -> Step:
+        stmt = select(Step).filter(Step.id == id)
+        result = await self.session.execute(stmt)
+        return_step = result.scalars().first()
+        if not return_step:
+            raise StepNotFoundException()
+        self.session.delete(return_step)
+        await self.session.commit()
+        return return_step
